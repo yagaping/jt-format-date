9474e45698b31746e18cb36a61e238147c73dd70
guop ping <yagaping@163.com>
2025-08-13 19:05:53 +0800

    输入当前时间

--- test.js
+++ test.js
@@ -3,3 +3,5 @@ const age = require('./index');
 console.log(age.getCurrentWeekday('2025-08-11')); // 输出当前星期几
 
 console.log(age.getCurrentTime()); // 输出当前时间
+
+console.log(age.currentAge('1992-09-26')); // 输出年龄
